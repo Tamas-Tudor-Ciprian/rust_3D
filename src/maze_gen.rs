2a8580a45e9cc380ef29f8818f30506f1d706acb
Tamas-Tudor-Ciprian <tamastudorciprian@gmail.com>
2026-05-07 22:06:29 +0300

    started working on random mazes

---
+++ src/maze_gen.rs
@@ -0,0 +1,41 @@
+
+
+let const WALL = "#";
+let const EMPTY = " ";
+
+
+
+
+fn maze_gen(width: usize, height : usize) -> Vec<String> {
+
+
+
+	let mut output = Vec![]
+
+	for i in range 0..height{
+		let mut row : String = "";
+		for j in range 0..width{
+				match (i,j){
+					
+
+					   }			
+					
+
+
+					}
+	
+
+				}
+
+
+
+
+
+
+
+
+
+	output
+
+
+							}	
