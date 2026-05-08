62304eb98fb7d6dc13618cf1db4af258370e9212
Tudor <uiv80988@vitesco.com>
2026-05-08 15:25:28 +0300

    boundary generation is functional

--- src/maze_gen.rs
+++ src/maze_gen.rs
@@ -1,33 +1,82 @@
-
-
-let const WALL = "#";
-let const EMPTY = " ";
-
+use std::thread::sleep;
+use std::time::Duration;
 
 
 
-fn maze_gen(width: usize, height : usize) -> Vec<String> {
+const WALL:&str = "#";
+const EMPTY:&str = " ";
+const HEIGHT:usize = 8;
+const WIDTH:usize = 16;
 
 
 
-	let mut output = Vec![]
+fn maze_boundary(width: usize, height : usize,maze: &mut Vec<String>) {
 
-	for i in range 0..height{
-		let mut row : String = "";
-		for j in range 0..width{
-				match (i,j){
-					
 
-					   }			
+
+	let mut output = maze.clone();
+
+	for i in 0..height{
+		let mut row= "".to_string();
+		for j in 0..width{
+				let ch = match (i,j){
+					(0,_) => WALL,
+					(_,0) => WALL,
+					(h,_) if h == height - 1 => WALL,
+					(_,w) if w == width - 1 => WALL,
+					_ => EMPTY,					
+					   };
+
+				row += ch;			
 					
 
 
 					}
+		output.push(row);
 	
 
 				}
 
 
+	*maze = output;
+
+}
+
+fn print_maze(height: usize, maze : &Vec<String>){
+
+
+	for i in 0..height{
+		println!("{}",maze[i]);
+	}
+}
+
+		
+fn add_maze_
+
+
+
+						
+
+
+fn main(){
+
+
+	let mut step = 0;
+
+
+	let mut maze = Vec::new();
+
+	println!("Generating the maze...");
+	sleep(Duration::from_secs(1));
+	//this will clear the screen
+	print!("\x1B[2J\x1B[H");
+
+
+
+	maze_layer_gen(HEIGHT,WIDTH,&mut maze);
+	println!("Step {}", step);
+	print_maze(HEIGHT,&maze);				
+}
 
 
 
@@ -35,7 +84,9 @@ fn maze_gen(width: usize, height : usize) -> Vec<String> {
 
 
 
-	output
 
 
-							}	
+
+
+
+
