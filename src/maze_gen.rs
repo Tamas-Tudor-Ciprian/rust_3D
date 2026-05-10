110aa24e34eeb8e1d35b71a6feb45ff80490398d
Tamas-Tudor-Ciprian <tamastudorciprian@gmail.com>
2026-05-11 00:50:12 +0300

    making progres on the maze algo but not quite there

--- src/maze_gen.rs
+++ src/maze_gen.rs
@@ -2,11 +2,20 @@ use std::thread::sleep;
 use std::time::Duration;
 
 
-
-const WALL:&str = "#";
-const EMPTY:&str = " ";
 const HEIGHT:usize = 8;
 const WIDTH:usize = 16;
+
+
+
+fn lcg(seed:&mut u64) ->u64{
+	*seed = seed.wrapping_mul(6364136223846793005).wrapping_add(543251323);
+	*seed
+}
+
+
+fn rand_range(seed: &mut u64, low:u64, high: u64) -> u64 {
+	low + (lcg(seed) % (high - low))
+}
 
 
 
@@ -22,8 +31,8 @@ fn maze_boundary(width: usize, height : usize,maze: &mut Vec<String>) {
 				let ch = match (i,j){
 					(0,_) => WALL,
 					(_,0) => WALL,
-					(h,_) if h == height - 1 => WALL,
-					(_,w) if w == width - 1 => WALL,
+					(h,_) if h == (height - 1) => WALL,
+					(_,w) if w == (width - 1) => WALL,
 					_ => EMPTY,					
 					   };
 
@@ -50,12 +59,76 @@ fn print_maze(height: usize, maze : &Vec<String>){
 	}
 }
 
+fn matrix(maze: &Vec<String>) -> Vec<Vec<char>> {
+    maze.iter()
+        .map(|row| row.chars().collect()).collect()
+}
+
+
+fn printable(maze: &Vec<Vec<char>>) -> Vec<String> {
+	
+	let mut output = Vec::new();
+
+
+	for row in maze {
+		let mut row_str = "".to_string();
+		for ch in row{
+			row_str += &ch.to_string();
+		}
+		output.push(row_str);
+	}
+
+	output
+
+
+
+}
+
 		
-fn add_maze_
+fn add_maze_level(maze: Vec<String>,seed : &mut u64) -> Vec<String>{
 
 
+	let WALL : char = '#';
+	let EMPTY : char = ' ';
 
-						
+	let mut matrix = matrix(&maze);
+
+	let height = matrix.len();
+	let width = matrix[0].len();
+
+	for i in 1..(height-1){
+		for j in 1..(width-1){
+			
+			if rand_range(seed,0,4) == 1 && matrix[i][j] == ' '{
+			
+			//this is the pattern that does all the magic for what I intended for this file
+			//the matrix elements in the tuple represent the cells bordering the current one
+			matrix[i][j] = match (matrix[i-1][j-1], //1
+					      matrix[i-1][j],   //2
+					      matrix[i-1][j+1], //3
+					      matrix[i][j-1],   //4
+					      matrix[i][j+1],   //5
+					      matrix[i+1][j-1], //6
+					      matrix[i+1][j],   //7
+					      matrix[i+1][j+1]){//8
+							(_,WALL,_,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY) => WALL,
+							(_,EMPTY,EMPTY,WALL,EMPTY,_,EMPTY,EMPTY) => WALL,
+							(EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,_,WALL,_) => WALL,
+							(EMPTY,EMPTY,_,EMPTY,WALL,EMPTY,EMPTY,_) => WALL,
+							_ => EMPTY,
+						};
+						}
+
+		
+					}
+
+				}
+
+
+	return printable(&matrix);
+
+
+}
 
 
 fn main(){
@@ -66,6 +139,10 @@ fn main(){
 
 	let mut maze = Vec::new();
 
+	let mut seed = 34527654321;
+
+
+
 	println!("Generating the maze...");
 	sleep(Duration::from_secs(1));
 	//this will clear the screen
@@ -73,20 +150,35 @@ fn main(){
 
 
 
-	maze_layer_gen(HEIGHT,WIDTH,&mut maze);
+	maze_boundary(WIDTH,HEIGHT,&mut maze);
 	println!("Step {}", step);
-	print_maze(HEIGHT,&maze);				
+	print_maze(HEIGHT,&maze);
+	sleep(Duration::from_secs(1));
+
+
+	print!("\x1B[2J\x1B[H");
+	
+	maze = add_maze_level(maze,&mut seed);
+	step +=1;
+	println!("Step {}", step);
+	print_maze(HEIGHT,&maze);
+	sleep(Duration::from_secs(1));
+	print!("\x1B[2J\x1B[H");
+	
+	maze = add_maze_level(maze,&mut seed);
+	step +=1;                
+	println!("Step {}", step);
+	print_maze(HEIGHT,&maze);
+
+
+	sleep(Duration::from_secs(1));
+	print!("\x1B[2J\x1B[H");
+	
+	maze = add_maze_level(maze,&mut seed);
+	step +=1;                
+	println!("Step {}", step);
+	print_maze(HEIGHT,&maze);
+
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
